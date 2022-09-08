8e650d5429950201333d608fee5d06114d03cf24
swayamraina <swayam.raina@swiggy.in>
2022-09-08 17:09:57 +0530

    update keys

--- src/main/java/com/hackathon/swiggy/health/vo/TimeSeries.java
+++ src/main/java/com/hackathon/swiggy/health/vo/TimeSeries.java
@@ -1,23 +1,12 @@
 package com.hackathon.swiggy.health.vo;
 
+import com.hackathon.swiggy.health.vo.timeseries.DayData;
+
 import java.util.Map;
 
 public class TimeSeries {
 
     // key = 2022-09-25
     public Map<String, DayData> dayData;
-
-}
-
-class DayData {
-
-    // key = 15
-    public Map<String, HourData> hourData;
-
-}
-
-class HourData {
-
-    public int steps;
 
 }
